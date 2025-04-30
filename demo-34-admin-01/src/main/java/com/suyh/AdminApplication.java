9524797dc5a2f4bf2a20264d4be50abd5a0d5c1c
suyh <suyunhong@ater-info.com>
2025-04-30 14:01:44 +0800

    1

---
+++ demo-34-admin-01/src/main/java/com/suyh/AdminApplication.java
@@ -0,0 +1,17 @@
+package com.suyh;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author suyh
+ * @since 2025-04-30
+ */
+@SpringBootApplication
+@EnableAdminServer
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
